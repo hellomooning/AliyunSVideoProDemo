62247d107084c71cacc57dcfe34d6bdeabf22681
xiao <wang@g.com>
2020-05-14 15:19:05 +0800

    sdk 3.16.0

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -11,24 +11,38 @@ export class HomePage {
 
 	constructor(private platform: Platform) { }
 
+	recordSetting() {
+		console.log("recordSetting");
+		if (this.platform.is("android")) {
+			cordova.plugins.AliyunSVideoPro.startRecordSetting('http://www.baidu.com');
+		}
+	}
+
+	cropSetting() {
+		console.log("cropSetting");
+		if (this.platform.is("android")) {
+			cordova.plugins.AliyunSVideoPro.startCropSetting();
+		}
+  }
+  
+	editSetting() {
+		console.log("editSetting");
+		if (this.platform.is("android")) {
+			cordova.plugins.AliyunSVideoPro.startEditSetting();
+		}
+	}
+
+	beauty() {
+		console.log("beauty");
+		if (this.platform.is("android")) {
+			cordova.plugins.AliyunSVideoPro.startBeauty();
+		}
+	}
+
 	record() {
 		console.log("record");
 		if (this.platform.is("android")) {
 			cordova.plugins.AliyunSVideoPro.startRecord();
-		}
-	}
-
-	crop() {
-		console.log("crop");
-		if (this.platform.is("android")) {
-			cordova.plugins.AliyunSVideoPro.startCrop();
-		}
-  }
-  
-	edit() {
-		console.log("edit");
-		if (this.platform.is("android")) {
-			cordova.plugins.AliyunSVideoPro.startEdit();
 		}
 	}
 }
